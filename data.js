ed76d528b8c1af4a994ca81333a2d91056ec4c0a
Reim Daniel <56229262+Danielmodding@users.noreply.github.com>
2022-05-28 18:54:17 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,222 @@
+var data = [
+   {
+      "title":"Mercedes Benz / Daimler",
+      "description":"http://rk.mb-qr.com/de/"
+   },
+   {
+      "title":"Maybach Daimler",
+       "description":"http://rk.mb-qr.com/de/240/"
+    },
+    {
+        "title": "Chrysler",
+        "description": "http://www.chrysler.de/informationen-fur-rettungskrafte.html"
+    },
+    {
+        "title": "Doge",
+        "description": "http://www.dodge.de/informationen-fur-rettungskrafte.html"
+    },
+    {
+        "title": "Lancia",
+        "description": "http://www.lancia.de/mopar/rettungsdatenblaetter"
+    },
+    {
+        "title": "BMW / AOS Group",
+        "description": "https://aos.bmwgroup.com/web/oss/service"
+    },
+    {
+        "title": "Suzuki",
+        "description": "https://auto.suzuki.de/service/dokumente-hilfe"
+    },
+    {
+        "title": "Peugeot",
+        "description": "https://peugeot.my-customerportal.com/peugeot/s/?language=de"
+    },
+    {
+        "title": "Citroen",
+        "description": "https://public.servicebox-parts.com/AC/secours/AC/documents/de_DE/index.html"
+    },
+    {
+        "title": "Arbarth",
+        "description": "https://www.abarth.de/rettungsdatenblaetter"
+    },
+    {
+        "title": "Alfaromeo",
+        "description": "https://www.alfaromeo.de/rettungsdatenblaetter"
+    },
+    {
+        "title": "Alpine",
+        "description": "https://www.alpinecars.com/alp-content/uploads/2018/08/Rettungskarte_Alpine_01_Ausgabe_07_2017_DE.pdf"
+    },
+    {
+        "title": "Astonmartin",
+        "description": "https://www.astonmartin.com/rescuesheets"
+    },
+    {
+        "title": "Audi",
+        "description": "https://www.audi.de/de/brand/de/beratung-kauf/sonderabnehmer/sonderfahrzeuge/einsatzfahrzeuge/leitfaden-fuer-rettungsdienste.html"
+    },
+    {
+        "title": "Bentley",
+        "description": "https://www.bentleymotors.com/en/world-of-bentley/ownership/services/other-vehicle-information.html#578e5b891d4509ffe09a0a67ca2f17cc"
+    },
+    {
+        "title": "Chevrolet",
+        "description": "https://www.chevrolet.de/de/rettungskarten"
+    },
+    {
+        "title": "CS Reisemobile",
+        "description": "https://www.cs-reisemobile.de/de/wohnmobile-service/rettungskarten.htm"
+    },
+    {
+        "title": "Cupra",
+        "description": "https://www.cupraofficial.at/service-zubehoer/downloads/rettungsdatenblaetter"
+    },
+    {
+        "title": "Dacia",
+        "description": "https://www.dacia.de/rettungskarten.html"
+    },
+    {
+        "title": "Daihatsu",
+        "description": "https://www.daihatsu.de/daihatsu_rettungsdatenblaetter_de_at.pdf"
+    },
+    {
+        "title": "Dethleffs",
+        "description": "https://www.dethleffs.de/infos/rettungskarten/rettungskarten/modelljahr-2021/"
+    },
+    {
+        "title": "DS Automobiles",
+        "description": "https://www.dsautomobiles.de/ds-services/rettungsdatenblaetter.html"
+    },
+    {
+        "title": "Fiat",
+        "description": "https://www.fiat.de/rettungsdatenblaetter"
+    },
+    {
+        "title": "Fiat Professional",
+        "description": "https://www.fiatprofessional.com/de/rettungsdatenblaetter"
+    },
+    {
+        "title": "Ford",
+        "description": "https://www.fordserviceinfo.com/Rescuecard"
+    },
+    {
+        "title": "Honda",
+        "description": "https://www.honda.de/cars/services/download-rettungsdatenblaetter.html"
+    },
+    {
+        "title": "Hyundai",
+        "description": "https://www.hyundai.de/service-zubehoer/gut-informiert/#Rettungsdatenbl%C3%A4tter"
+    },
+    {
+        "title": "Opel Vauxhall",
+        "description": "https://www.ifz-berlin.de/index.html#/rescue"
+    },
+    {
+        "title": "Opel",
+        "description": "https://www.ifz-berlin.de/index.html#/rescue"
+    },
+    {
+        "title": "Vauxhall",
+        "description": "https://www.ifz-berlin.de/index.html#/rescue"
+    },
+    {
+        "title": "Isuzu",
+        "description": "https://www.isuzu-sales.de/service/rettungsdatenblaetter/"
+    },
+    {
+        "title": "Jaguar",
+        "description": "https://www.jaguar.de/service-und-zubehor/service-garantien/rettungskarten.html"
+    },
+    {
+        "title": "Jeep",
+        "description": "https://www.jeep.de/rettungsdatenblatter"
+    },
+    {
+        "title": "Kia",
+        "description": "https://www.kia.com/de/service/rettungsdienste/"
+    },
+    {
+        "title": "Landrover",
+        "description": "https://www.landrover.de/ownership/emergency.html"
+    },
+    {
+        "title": "Rangerover",
+        "description": "https://www.landrover.de/ownership/emergency.html"
+    },
+    {
+        "title": "Lexus",
+        "description": "https://www.lexus.de/ownership/about-my-lexus/fahrzeuginformationen/#rescue"
+    },
+    {
+        "title": "Maserati",
+        "description": "https://www.maserati.com/de/de/Aftersales/services"
+    },
+    {
+        "title": "Mazda",
+        "description": "https://www.mazda.de/rettungskarten/"
+    },
+    {
+        "title": "Mitsubishi",
+        "description": "https://www.mitsubishi-publikationen.de/de/profiles/bf834dc9c730/editions/category/2756"
+    },
+    {
+        "title": "Nissan",
+        "description": "https://www.nissan.de/rescuers_page.html"
+    },
+    {
+        "title": "Porsche",
+        "description": "https://www.porsche.com/germany/accessoriesandservices/porscheservice/vehicleinformation/documents/"
+    },
+    {
+        "title": "Renault",
+        "description": "https://www.renault.de/tipps-und-anleitungen/rettungskarten.html"
+    },
+    {
+        "title": "Ruf",
+        "description": "https://www.ruf-automobile.de/rettungskarten/"
+    },
+    {
+        "title": "Seat VAG",
+        "description": "https://www.seat.at/service-zubehoer/informationen-downloads/unfallrettung"
+    },
+    {
+        "title": "Skoda VAG",
+        "description": "https://www.skoda-auto.de/service/skoda-sicherheit"
+    },
+    {
+        "title": "Ssangyong",
+        "description": "https://www.ssangyong.de/service/rettungsdatenblaetter"
+    },
+    {
+        "title": "Streetscooter",
+        "description": "https://www.streetscooter.com/de/rettungskarten/"
+    },
+    {
+        "title": "Deutsche Post",
+        "description": "https://www.streetscooter.com/de/rettungskarten/"
+    },
+    {
+        "title": "Subaru",
+        "description": "https://www.subaru.de/marke-technik/technik/sicherheit"
+    },
+    {
+        "title": "Sunlight",
+        "description": "https://www.sunlight.de/downloads/"
+    },
+    {
+        "title": "Tesla",
+        "description": "https://www.tesla.com/de_DE/firstresponders"
+    },
+    {
+        "title": "Toyota",
+        "description": "https://www.toyota.de/service_und_zubehoer/fahrzeuginformationen/technische_unterstuetzung/rettungsdatenblaetter"
+    },
+    {
+        "title": "VW Volkswagen VAG",
+        "description": "https://www.volkswagen.de/de/besitzer-und-nutzer/wichtige-kundeninformationen/rechtliches/rescue-data.html"
+    },
+    {
+        "title": "Volvo",
+        "description": "https://www.volvocars.com/de/zubehoer-und-services/dokumente-und-hilfe/rettungsleitfaeden"
+    }
+]
